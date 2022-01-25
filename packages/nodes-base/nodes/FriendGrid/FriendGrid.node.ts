cfe63ffd5ae54f76664f27d57fd87611aaf6c3df
Treuze <20151955@academia.uatlantica.pt>
2022-01-25 20:59:40 +0000

    test Module

---
+++ packages/nodes-base/nodes/FriendGrid/FriendGrid.node.ts
@@ -0,0 +1,69 @@
+import {
+    IExecuteFunctions,
+} from 'n8n-core';
+
+import {
+    IDataObject,
+    INodeExecutionData,
+    INodeType,
+    INodeTypeDescription,
+} from 'n8n-workflow';
+
+import {
+    OptionsWithUri,
+} from 'request';
+
+export class FriendGrid implements INodeType {
+    description: INodeTypeDescription = {
+        displayName: 'FriendGrid',
+        name: 'friendGrid',
+        icon: 'file:friendGrid.svg',
+        group: ['transform'],
+        version: 1,
+        description: 'Consume FriendGrid API',
+        defaults: {
+            name: 'FriendGrid',
+            color: '#1A82e2',
+        },
+        inputs: ['main'],
+        outputs: ['main'],
+        credentials: [
+            {
+	name: 'friendGridApi',
+	required: true,
+}
+
+        ],
+        properties: [
+			{
+				displayName: 'Protocol',
+				name: 'protocol',
+				type: 'options',
+				options: [
+					{
+						name: 'FTP',
+						value: 'ftp',
+					},
+					{
+						name: 'SFTP',
+						value: 'sftp',
+					},
+				],
+				default: 'ftp',
+				description: 'File transfer protocol.',
+			}
+		],     
+    };
+
+    async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+        //this.getNodeParameter('protocol',0) as string;
+        //const credentials = await this.getCredentials('friendGridApi') as IDataObject;
+        //return [this.helpers.returnJsonArray({resource:resource, credentials:credentials})];
+
+        return []
+    }
+}
+
+ 
+
+ 
